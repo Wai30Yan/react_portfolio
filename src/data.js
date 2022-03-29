ad4dd9504172c0d0142e43550d1bdb91cf3b0347
Wai30Yan <42363348+Wai30Yan@users.noreply.github.com>
2022-03-29 12:45:11 +1100

    updated data.js file

--- src/data.js
+++ src/data.js
@@ -1,5 +1,7 @@
 import joke from './images/joke.png'
-import todolist from './images/todolist.png'
+import todo from './images/todo.png'
+import movie from './images/movie.png'
+import voice from './images/voice.png'
 
 export const projects = [
     {
@@ -14,30 +16,31 @@ export const projects = [
     },
     {
         id: 2,
-        img: joke,
+        img: voice,
         name: "Note Taking App",
         demoLink: "https://wai30yan.github.io/react_joke_app/",
-        githubLink: "https://github.com/Wai30Yan/react_joke_app",
+        githubLink: "https://github.com/Wai30Yan/mern_speech_to_text",
         desc: "This is my Engineering Capstone Project and is not fully finished yet. It's a note taking app with Speech Recognition. " +
         "It is a fullstack app developed using MERN stack with REST API. Json Web Token is used in Express server for Authorization and " +
         "Authentication, and Redux with Redux-Thunk for state management at client side.",
     },
     {
         id: 3,
-        img: todolist,
-        name: "TodoList App",
+        img: movie,
+        name: "Movie App",
+        demoLink: "https://wai30yan.github.io/react_movie_app/",
+        githubLink: "https://github.com/Wai30Yan/react_movie_app",
+        desc: "A simple React app that fetches movie data from Rapid API and renders it on DOM." +
+        "I made this app to test out Tailwind CSS for UI design.", 
+    },
+    {
+        id: 4,
+        img: todo,
+        name: "Todo App (Firebase Backend)",
         demoLink: "https://wai30yan.github.io/react_firebase_todo/",
         githubLink: "https://github.com/Wai30Yan/react_firebase_todo",
         desc: "This is a mini fullstack application that can handle CRUD operations." + 
         "Firebase is used for backend and database service." + 
         "Material UI is used for styling. It also show the date and time of when a task is added to the TodoList.",
-    },
-    {
-        id: 4,
-        img: todolist,
-        name: "Movie App",
-        demoLink: "https://wai30yan.github.io/react_firebase_todo/",
-        githubLink: "https://github.com/Wai30Yan/react_firebase_todo",
-        desc: "",
     },
 ]
